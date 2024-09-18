58646a6c4fc1dbf10e35deea058a4a76942a185f
David Farcas <>
2024-09-18 08:58:37 +0300

    If a 0% tip is selected, the text will become red.

--- WeSplit/WeSplit/ContentView.swift
+++ WeSplit/WeSplit/ContentView.swift
@@ -55,6 +55,7 @@ struct ContentView: View {
                     Picker("Tip percentage", selection: $tipPercentage) {
                         ForEach(0..<101) {
                             Text($0, format: .percent)
+                                .foregroundStyle($0 == 0 ? .red : .primary)
                         }
                     }
                     .pickerStyle(.navigationLink)
